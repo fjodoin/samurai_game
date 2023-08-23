fb0f800f5318c1376ee93566200b12b75f6ed681
fjodoin <fjodoin@protonmail.com>
2023-08-23 17:25:59 -0400

    1 v 1 rectangles operational (no attacks yet - no boarders either.

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ class Sprite {
 		this.position = position	// position of Sprite
 		this.velocity = velocity	// speed of Sprite
 		this.height = 150			// static value from height of Sprite draw()
+		this.lastKey
 	}
 	
 	// Draw Sprite on canvas
@@ -26,6 +27,7 @@ class Sprite {
 	// Update position of Sprite
 	update() {
 		this.draw()
+		this.position.x += this.velocity.x
 		this.position.y += this.velocity.y
 
 		if (this.position.y + this.height + this.velocity.y >= canvas.height){
@@ -62,6 +64,22 @@ const enemy = new Sprite({
 	}	
 })
 
+const keys = {
+	a: {
+		pressed: false
+	},
+	d: {
+		pressed: false
+	},
+	ArrowRight: {
+		pressed: false
+	},
+	ArrowLeft: {
+		pressed: false
+	}
+}
+let lastKey
+
 // This method is necessary for making the game dynamic - "frame-by-frame"
 function animate(){
 	window.requestAnimationFrame(animate)
@@ -69,8 +87,79 @@ function animate(){
 	c.fillRect(0, 0, canvas.width, canvas.height) // Clear frame to avoid "paint drip" effect
 	player.update()
 	enemy.update()
+
+	// player movement
+	player.velocity.x = 0
+	if (keys.a.pressed && lastKey === 'a') {
+		player.velocity.x = -1
+	} else if (keys.d.pressed && lastKey === 'd') {
+		player.velocity.x = 1
+	}
+
+	// enemy movement
+	enemy.velocity.x = 0
+	if (keys.ArrowLeft.pressed && enemy.lastKey === 'ArrowLeft') {
+		enemy.velocity.x = -1
+	} else if (keys.ArrowRight.pressed && enemy.lastKey === 'ArrowRight') {
+		enemy.velocity.x = 1
+	}
 }
 
 animate()
 
-// Testing ssh keys...
+// Keydown event logic
+window.addEventListener('keydown', (event) => {
+	switch (event.key){
+		// player keystrokes
+		case 'a':
+			keys.a.pressed = true	// move player to the left
+			lastKey = 'a'
+			break
+		case 'd':
+			keys.d.pressed = true	// move player to the right
+			lastKey = 'd'
+			break
+		case 'w':
+			player.velocity.y = -5
+			break
+		
+		// enemy keystrokes
+		case 'ArrowLeft':
+			keys.ArrowLeft.pressed = true	// move player to the left
+			enemy.lastKey = 'ArrowLeft'
+			break
+		case 'ArrowRight':
+			keys.ArrowRight.pressed = true	// move player to the right
+			enemy.lastKey = 'ArrowRight'
+			break
+		case 'ArrowUp':
+			enemy.velocity.y = -5
+			break
+		
+	}
+	console.log(event)
+})
+
+// Keyup event logic
+window.addEventListener('keyup', (event) => {
+	// player keyups
+	switch (event.key){
+		case 'a':
+			keys.a.pressed = false
+			break
+		case 'd':
+			keys.d.pressed = false
+			break
+	}
+
+	// Enemy keyups
+	switch (event.key) {
+		case 'ArrowLeft':
+			keys.ArrowLeft.pressed = false
+			break
+		case 'ArrowRight':
+			keys.ArrowRight.pressed = false
+			break		
+	}
+	console.log(event)
+})
